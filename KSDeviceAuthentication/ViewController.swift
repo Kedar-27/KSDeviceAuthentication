b19d8e7bdf461a1119db3ebca4f9417db41bb910
Kedar27 <kedarjsukerkar@yahoo.co.in>
2019-11-22 16:13:35 +0530

    project setup done

---
+++ KSDeviceAuthentication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KSDeviceAuthentication
+//
+//  Created by Kedar Sukerkar on 22/11/19.
+//  Copyright © 2019 Kedar Sukerkar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
