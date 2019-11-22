d2779f4376a3536792ecf444fb5aeb39355d92e0
Kedar27 <kedarjsukerkar@yahoo.co.in>
2019-11-22 17:11:54 +0530

    KSDeviceAuthenticator added

--- KSDeviceAuthentication/ViewController.swift
+++ KSDeviceAuthentication/ViewController.swift
@@ -7,14 +7,294 @@
 //
 
 import UIKit
+import ObjectiveC.NSObjCRuntime
+
 
 class ViewController: UIViewController {
 
+    // MARK: - Constants
+    static let storyboardIdentifier = "ViewController"
+    static let storyboardName       = "Main"
+    
+    // MARK: - Outlets
+    @IBOutlet weak var deviceAuthenticationButton: UIButton!
+    @IBOutlet weak var biometricAuthenticationButton: UIButton!
+    
+    // MARK: - Properties
+    
+    
+    
+    // MARK: - Data Injections
+    
+    
+    
+    // MARK: - UIViewController
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.setupVC()
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.setupUI()
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.view.endEditing(true)
+    }
+    
+    func setupVC(){
+        
+        
+        
+        
+    }
+    
+    func setupUI(){
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+    // MARK: - IBActions
+    @IBAction func deviceAuthenticationButtonClicked(_ sender: Any) {
+        // start authentication
+        
+        KSDeviceAuthenticator.authenticateWithPasscode(reason: "") {[weak self] (result) in
+            switch result {
+            case .success( _):
+                
+                // authentication successful
+                self?.showLoginSucessAlert()
+                
+            case .failure(let error):
+                
+                switch error {
+                    
+                // device does not support biometric (face id or touch id) authentication
+                case .biometryNotAvailable:
+                    self?.showErrorAlert(message: error.message())
+                    
+                // No biometry enrolled in this device, ask user to register fingerprint or face
+                case .biometryNotEnrolled:
+                    self?.showGotoSettingsAlert(message: error.message())
+                    
+                // show alternatives on fallback button clicked
+                case .fallback:
+                     // enter username password manually
+                    
+                    break
+                    
+                    // Biometry is locked out now, because there were too many failed attempts.
+                // Need to enter device passcode to unlock.
+                case .biometryLockout:
+                    self?.showPasscodeAuthentication(message: error.message())
+                    
+                // do nothing on canceled by system or user
+                case .canceledBySystem, .canceledByUser:
+                    break
+                    
+                // show error for any other reason
+                default:
+                    self?.showErrorAlert(message: error.message())
+                }
+            }
+            
+            
+            
+            
+        }
+        
+    }
+    
+    
+    @IBAction func biometricAuthenticationButtonClicked(_ sender: Any) {
+        // Set AllowableReuseDuration in seconds to bypass the authentication when user has just unlocked the device with biometric
+        KSDeviceAuthenticator.shared.allowableReuseDuration = 30
+        
+        
+
+        // start authentication
+        KSDeviceAuthenticator.authenticateWithBioMetrics(reason: "") { [weak self] (result) in
+                
+            switch result {
+            case .success( _):
+                
+                // authentication successful
+                self?.showLoginSucessAlert()
+                
+            case .failure(let error):
+                
+                switch error {
+                    
+                // device does not support biometric (face id or touch id) authentication
+                case .biometryNotAvailable:
+                    self?.showErrorAlert(message: error.message())
+                    
+                // No biometry enrolled in this device, ask user to register fingerprint or face
+                case .biometryNotEnrolled:
+                    self?.showGotoSettingsAlert(message: error.message())
+                    
+                // show alternatives on fallback button clicked
+                case .fallback:
+                     // enter username password manually
+                    
+                    break
+                    
+                    // Biometry is locked out now, because there were too many failed attempts.
+                // Need to enter device passcode to unlock.
+                case .biometryLockout:
+                    self?.showPasscodeAuthentication(message: error.message())
+                    
+                // do nothing on canceled by system or user
+                case .canceledBySystem, .canceledByUser:
+                    break
+                    
+                // show error for any other reason
+                default:
+                    self?.showErrorAlert(message: error.message())
+                }
+            }
+        }
+        
+        
+    }
+    
+    // show passcode authentication
+    func showPasscodeAuthentication(message: String) {
+        
+        KSDeviceAuthenticator.authenticateWithPasscode(reason: message) { [weak self] (result) in
+            switch result {
+            case .success( _):
+                self?.showLoginSucessAlert() // passcode authentication success
+            case .failure(let error):
+                print(error.message())
+            }
+        }
+    }
+    
+    
 
 
 }
 
+// MARK: - Alerts
+extension ViewController {
+    
+    func showAlert(title: String, message: String) {
+        
+        let okAction = AlertAction(title: OKTitle)
+        let alertController = getAlertViewController(type: .alert, with: title, message: message, actions: [okAction], showCancel: false) { (button) in
+        }
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    func showLoginSucessAlert() {
+        showAlert(title: "Success", message: "Login successful")
+    }
+    
+    func showErrorAlert(message: String) {
+        showAlert(title: "Error", message: message)
+    }
+    
+    func showGotoSettingsAlert(message: String) {
+        let settingsAction = AlertAction(title: "Go to settings")
+        
+        let alertController = getAlertViewController(type: .alert, with: "Error", message: message, actions: [settingsAction], showCancel: true, actionHandler: { (buttonText) in
+            if buttonText == CancelTitle { return }
+            
+            // open settings
+            let url = URL(string: UIApplication.openSettingsURLString)
+            if UIApplication.shared.canOpenURL(url!) {
+                UIApplication.shared.open(url!, options: [:])
+            }
+            
+        })
+        present(alertController, animated: true, completion: nil)
+    }
+}
+
+
+let CancelTitle     =   "Cancel"
+let OKTitle         =   "OK"
+typealias AlertViewController = UIAlertController
+
+struct AlertAction {
+    
+    var title: String = ""
+    var type: UIAlertAction.Style? = .default
+    var enable: Bool? = true
+    var selected: Bool? = false
+    
+    init(title: String, type: UIAlertAction.Style? = .default, enable: Bool? = true, selected: Bool? = false) {
+        self.title = title
+        self.type = type
+        self.enable = enable
+        self.selected = selected
+    }
+}
+
+extension UIViewController {
+    
+    // Show Alert or Action sheet
+    func getAlertViewController(type: UIAlertController.Style, with title: String?, message: String?, actions:[AlertAction], showCancel: Bool , actionHandler:@escaping ((_ title: String) -> ())) -> AlertViewController {
+        
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: type)
+        
+        // items
+        var actionItems: [UIAlertAction] = []
+        
+        // add actions
+        for (index, action) in actions.enumerated() {
+            
+            let actionButton = UIAlertAction(title: action.title, style: action.type!, handler: { (actionButton) in
+                actionHandler(actionButton.title ?? "")
+            })
+            
+            actionButton.isEnabled = action.enable!
+            if type == .actionSheet { actionButton.setValue(action.selected, forKey: "checked") }
+            actionButton.setAssociated(object: index)
+            
+            actionItems.append(actionButton)
+            alertController.addAction(actionButton)
+        }
+        
+        // add cancel button
+        if showCancel {
+            let cancelAction = UIAlertAction(title: CancelTitle, style: .cancel, handler: { (action) in
+                actionHandler(action.title!)
+            })
+            alertController.addAction(cancelAction)
+        }
+        return alertController
+    }
+}
+
+extension UIAlertController {
+}
+
+
+/// NSObject associated object
+public extension NSObject {
+    
+    /// keys
+    private struct AssociatedKeys {
+        static var descriptiveName = "associatedObject"
+    }
+    
+    /// set associated object
+    @objc func setAssociated(object: Any) {
+        objc_setAssociatedObject(self, &AssociatedKeys.descriptiveName, object, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+    }
+    
+    /// get associated object
+    @objc func associatedObject() -> Any? {
+        return objc_getAssociatedObject(self, &AssociatedKeys.descriptiveName)
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,183 @@
-# KSDeviceAuthentication
+# KSDeviceAuthenticator
+
+KSDeviceAuthenticator provides wrapper around LocalAuthentication.framework for authentication device using touchId , faceId and passcode.
+
+
+**Note:** - Face ID authentication requires user's permission to be add in info.plist.
+```swift
+<key>NSFaceIDUsageDescription</key>
+<string>This app requires Face ID permission to authenticate using Face recognition.</string>
+```
+## Installation
+
+### CocoaPods
+
+To be added soon
+
+### Manual
+
+Drag and drop files from Sources folder into your project
+
+## Usage
+
+### Authenticate with biometric
+
+```swift
+KSDeviceAuthenticator.authenticateWithBioMetrics(reason: "") { (result) in
+
+    switch result {
+    case .success( _):
+        print("Authentication Successful")
+    case .failure(let error):
+        print("Authentication Failed")
+    }
+}
+```
+- When reason specified as empty - default will be used based on the device. Ex. for iPhone X - **"Confirm your face to authenticate."**,  For other devices - **"Confirm your fingerprint to authenticate."**
+
+### Can Authenticate with biometric
+
+- Alternatively you can check before authentication by following. This will check that if device supports Touch ID or Face ID authentication and your app can use that as of now.
+
+```swift
+if KSDeviceAuthenticator.canAuthenticate() {
+
+    KSDeviceAuthenticator.authenticateWithBioMetrics(reason: "") { (result) in
+        // check result -> success or failure
+    }
+}
+```
+### Check for Face ID
+- Check if device supports face recognition or not.
+```swift
+if KSDeviceAuthenticator.shared.faceIDAvailable() {
+    // device supports face id recognition.
+}
+```
+### Check for Touch ID
+- Check if device supports touch id authentication or not.
+```swift
+if BioMetricAuthenticator.shared.touchIDAvailable() {
+    // device supports touch id authentication
+}
+```
+
+### Fallback Reason
+- Fallback reason title will be shown when first authentication attempt is failed.
+- You can give alternate authentication options like enter 'username & password' when user clicks on fallback button.
+- Default reason is empty, which will hide fallback button.
+
+```swift
+KSDeviceAuthenticator.authenticateWithBioMetrics(reason: "Biometric Authentication", fallbackTitle: "Enter Credentials") { (result) in
+
+    switch result {
+    case .success( _):
+        // proceed further
+
+    case .failure(let error):
+
+        switch error {
+        case .fallback:
+
+            print("Authentication Failed")
+
+            // show alternatives on fallback button clicked
+            // for ex. - enter username/email and password
+
+        default:
+            break
+        }
+    }
+}
+```
+
+### BiometryLockedout
+- When biometry authentication is locked out after multiple failed attempts. You can unlock it by passcode authentication.
+- Provide your own passcode authentication reason here, default will be used if not provided.
+
+```swift
+KSDeviceAuthenticator.authenticateWithPasscode(reason: message) { (result) in
+    switch result {
+    case .success( _):
+        // passcode authentication success
+    case .failure(let error):
+        print(error.message())
+    }
+}
+```
+
+### Error Handling
+- There are various cases when biometric authentication can be failed.
+
+1. **fallback**
+    - Called when user clicks on provided fallback button.
+2. **biometryNotEnrolled**
+    - Called when no fingerprints or face is registered with the device.
+    - You can show message to register a new face or fingerprint here.
+    - Default message will be shown if not provided.
+3. **canceledByUser**
+    - Called when authentication canceled by user.
+4. **canceledBySystem**
+    - Called when authentication canceled by system when app goes into background or any other reason.
+5. **passcodeNotSet**
+    - Called when device passcode is not set and trying to use biometry authentication.
+    - We can ask user to set device passcode here by opening Settings Application.
+6. **failed**
+    - Called when multiple failed attempts made by user.
+    - You can show error message to user here.
+    - Default message can be shown if not provided.
+7. **biometryLockedout**
+    - Called when more than 5 failed attempts made using biometric authentication. This will locked your biometry system.
+    - You'll restrict user when this error is occured.
+    - Aleternatively you can ask user to enter device passcode to unlock biometry.
+8. **biometryNotAvailable**
+    - Called when device does not support Face ID or Touch ID authentication.
+
+### Example
+
+```swift
+KSDeviceAuthenticator.authenticateWithBioMetrics(reason: "") { [weak self] (result) in
+
+    switch result {
+    case .success( _):
+
+        // authentication successful
+        self?.showLoginSucessAlert()
+
+    case .failure(let error):
+
+        switch error {
+
+        // device does not support biometric (face id or touch id) authentication
+        case .biometryNotAvailable:
+            self?.showErrorAlert(message: error.message())
+
+        // No biometry enrolled in this device, ask user to register fingerprint or face
+        case .biometryNotEnrolled:
+            self?.showGotoSettingsAlert(message: error.message())
+
+        // show alternatives on fallback button clicked
+        case .fallback:
+            self?.txtUsername.becomeFirstResponder() // enter username password manually
+
+        // Biometry is locked out now, because there were too many failed attempts.
+        // Need to enter device passcode to unlock.
+        case .biometryLockout:
+            self?.showPasscodeAuthentication(message: error.message())
+
+        // do nothing on canceled by system or user
+        case .canceledBySystem, .canceledByUser:
+            break
+
+        // show error for any other reason
+        default:
+            self?.showErrorAlert(message: error.message())
+        }
+    }
+}
+```
+
+
+## License
+
+KSDeviceAuthentication is released under the MIT license.
